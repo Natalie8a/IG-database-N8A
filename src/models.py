01208e708b1798cd3dd90d8d9d7f26fb2c3f7d6a
Natalie8a <naty.8a@outlook.com>
2025-06-26 17:50:39 +0000

    good, just follow missing and I'm done, just have to go now

--- src/models.py
+++ src/models.py
@@ -1,15 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Date
+from datetime import date
+from sqlalchemy import String, Boolean, Integer, ForeignKey, Date, Enum
+import enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean(), nullable=False, default=True)
 
     def serialize(self):
         return {
@@ -17,3 +23,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'))
+    date: Mapped[str] = mapped_column(Date, default=date.today)
+    message: Mapped[str] = mapped_column(String(400), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            'user_id': self.user_id,
+            "date": self.date,
+            "message": self.message,
+        }
+
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(400), nullable=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey('post.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+
+        }
+
+
+class MediaType(enum.Enum):
+    IMAGE = "image"
+    VIDEO = "video"
+    Audio = "audio"
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    type: Mapped[MediaType] = mapped_column(Enum(MediaType), nullable=False)
